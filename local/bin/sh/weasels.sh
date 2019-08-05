b7a8bd7f78123b05b2a1522952018f2dc9e51ea5
Kaylyn Sigler <kaylyn.sigler@datadoghq.com>
2019-08-05 14:15:09 -0600

    hack a dog stuff

---
+++ local/bin/sh/weasels.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ 
+weasels="many|various|very|fairly|several|extremely\
+|exceedingly|quite|remarkably|few|surprisingly\
+|mostly|largely|huge|tiny|((are|is) a number)\
+|excellent|interestingly|significantly\
+|substantially|clearly|vast|relatively|completely"
